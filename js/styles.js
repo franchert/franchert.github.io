63a4f26fd0eb9f386ff707ddd487611893e9f710
vagrant <vagrant@localhost>
2015-05-14 22:14:24 +0000

    slimming

--- js/styles.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-$("#toggle").click(function(n){
-	n.preventDefault();
-	$("#toggle").parent(".wrapper").toggleClass("open");
-});
-
-});
--- styles.php
+++
@@ -1,9 +0,0 @@
-	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'/>
-	<link href='http://fonts.googleapis.com/css?family=Istok+Web:700' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Russo+One' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="/css/reset.css"/>
-	<link rel="stylesheet" href="/foundation/css/foundation.css" />	
-	<link rel="stylesheet" type="text/css" href="/css/global.css"/>
-	<?php if($page != ""){	?>
-		<link rel="stylesheet" type="text/css" href="/css/<?php echo $page ?>.css"/>	
-	<?php } ?>
